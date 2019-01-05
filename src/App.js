380cd40c19681255b1b0fc2f0d3e68a0236f6121
Krzysiek Szporer <krzysiek.szporer@gmail.com>
2019-01-05 17:06:29 +0100

    Added hot reload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React from "react";
+import { hot } from 'react-hot-loader'
 
 class App extends React.Component {
-    state = {
-        count: 0
-    }
+  state = {
+    count: 0
+  };
 
-    render() {
-        return (
-            <div>
-                <h1>Hello World!</h1>
-                <h2>Count: {this.state.count}</h2>
-                <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
-                <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <h2 className={this.state.count > 10 ? 'warning' : ''}>
+            Count: {this.state.count}
+        </h2>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
+        >
+          -
+        </button>
+      </div>
+    );
+  }
 }
 
-export default App
+export default hot(module)(App)
