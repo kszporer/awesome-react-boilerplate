2a2677c0e03594700360e2dc4081a8e22979bfe5
Krzysiek Szporer <krzysiek.szporer@gmail.com>
2019-01-05 20:19:44 +0100

    Configured Jest for testing the application

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import React from "react";
-import { hot } from 'react-hot-loader'
+import { hot } from "react-hot-loader";
+// import Warning from "./Warning";
+
+const Warning = React.lazy(() => import("./Warning"));
 
 class App extends React.Component {
   state = {
     count: 0
   };
 
+  increment = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState(state => ({ count: state.count - 1 }));
+  };
+
   render() {
+    const { count } = this.state;
     return (
       <div>
         <h1>Hello World!</h1>
-        <h2 className={this.state.count > 10 ? 'warning' : ''}>
-            Count: {this.state.count}
-        </h2>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
-        >
-          -
-        </button>
+        <h2 className={count > 10 ? "warning" : ""}>Count: {count}</h2>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
       </div>
     );
   }
 }
 
-export default hot(module)(App)
+export default hot(module)(App);
