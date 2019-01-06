755284746c03a398ea0c0dc21869fe1b6bb18af1
Krzysiek Szporer <krzysiek.szporer@gmail.com>
2019-01-06 17:44:00 +0100

    Added react-axe for chceking the accessibility issues in development

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from './App'
 import './styles.css'
 import DefaultErrorBoundary from './DefaultErrorBoundary'
 
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <DefaultErrorBoundary>
